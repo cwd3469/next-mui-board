205b47840a68bcd4093ebdc73b5bc98d15b4bfd4
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2022-12-27 17:00:52 +0900

    PTD-188 Feat : WConfirm 컴포넌트 개발
    WConfirm 컴포넌트 개발 : 공통 confirm 모달 컴포넌트 개발

---
+++ components/common/modals/WConfirm.tsx
@@ -0,0 +1,78 @@
+import { Stack } from '@mui/material';
+import { ModalButton, WDialogActions } from './styled';
+import WModal from './WModal';
+import { WConfirmProps } from './types';
+
+export const WConfirm = (props: WConfirmProps) => {
+  const {
+    // modal
+    handleClose,
+    subTitle,
+    title,
+    open,
+    children,
+    maxWidth,
+    bgDisable,
+    closeBtnOn,
+    titleSx,
+    style,
+    // alert
+    btnTitle,
+    btnColor,
+    btnTextColor,
+    disabled,
+    handleEvent,
+    // confirm
+    closeBtnEvent,
+    closeBtnTitle,
+    closeBtnColor,
+  } = props;
+
+  return (
+    <WModal
+      handleClose={handleClose}
+      subTitle={subTitle}
+      title={title}
+      open={open}
+      maxWidth={maxWidth}
+      bgDisable={bgDisable}
+      closeBtnOn={closeBtnOn}
+      titleSx={titleSx}
+      style={style}
+    >
+      <>
+        {children}
+        <WDialogActions>
+          <ModalButton
+            variant="contained"
+            color="info"
+            onClick={closeBtnEvent ? closeBtnEvent : handleClose}
+            sx={{
+              backgroundColor: closeBtnColor ? closeBtnColor : '#c1c1c1',
+            }}
+          >
+            {closeBtnTitle ? closeBtnTitle : '닫기'}
+          </ModalButton>
+          <ModalButton
+            disabled={disabled ? disabled : false}
+            variant="contained"
+            color="info"
+            onClick={handleEvent ? handleEvent : handleClose}
+            sx={{
+              backgroundColor: btnColor,
+              color: btnTextColor ? btnTextColor : '#fff',
+            }}
+          >
+            {btnTitle ? btnTitle : '확인'}
+          </ModalButton>
+        </WDialogActions>
+      </>
+    </WModal>
+  );
+};
+
+WConfirm.defaultProps = {
+  open: false,
+};
+
+export default WConfirm;
