a1aff02ee5a732bd0833af992e2dd1879583c93b
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2022-12-27 16:23:46 +0900

    PTD-188 Feat : WLayout 컴포넌트 개발
    WLayout 컴포넌트 개발 : 공통 레이아웃 컴포넌트 개발

---
+++ components/common/layouts/WLayout.tsx
@@ -0,0 +1,38 @@
+import { SxProps, Theme } from '@mui/material';
+import WauLogo from 'public/assets/logo/logo_login.png';
+import Image from 'next/image';
+import { Container, FlexCenter, ImageBox } from './styled';
+
+interface LogoType {
+  width?: string;
+}
+
+export const Logo = (props: LogoType) => {
+  const { width } = props;
+  return (
+    <ImageBox width={width ? width : '130px'}>
+      <Image src={WauLogo} alt="로고" layout="fill" />
+    </ImageBox>
+  );
+};
+
+export const WLayout = (props: {
+  children: JSX.Element | JSX.Element[] | React.ReactNode;
+  bg?: string;
+  containerColor?: string;
+  sx?: SxProps<Theme>;
+}) => {
+  const { children, bg, containerColor, sx } = props;
+  return (
+    <FlexCenter sx={{ backgroundColor: bg ? bg : '#fff' }}>
+      <Container
+        sx={{
+          backgroundColor: containerColor ? containerColor : '#fff',
+          ...sx,
+        }}
+      >
+        {children}
+      </Container>
+    </FlexCenter>
+  );
+};
