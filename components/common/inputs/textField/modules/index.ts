a7231e5ddfec9f89e5dfd51b3a45368a5cd84d20
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-10 20:50:47 +0900

    PTD-192 Feat : common > inputs > textField > modules > index 파일 개발
    common > inputs > textField > modules > index 파일 개발 : TextField 정리

---
+++ components/common/inputs/textField/modules/index.ts
@@ -0,0 +1,21 @@
+import WPwTextField from './WPwTextField';
+import WRepwTextField from './WRepwTextField';
+import WUseridTextField from './WUseridTextField';
+import WPharmcyNameTextField from './WPharmcyNameTextField';
+import WAddressDetailTextField from './WAddressDetailTextField';
+import WPhoneTextField from './WPhoneTextField';
+import WFaxTextField from './WFaxTextField';
+import WBusinessNumTextField from './WBusinessNumTextField';
+import WAdminNameTextField from './WAdminNameTextField';
+
+export {
+  WPwTextField,
+  WRepwTextField,
+  WUseridTextField,
+  WPharmcyNameTextField,
+  WAddressDetailTextField,
+  WPhoneTextField,
+  WFaxTextField,
+  WBusinessNumTextField,
+  WAdminNameTextField,
+};
