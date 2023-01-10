2840fe75c10822916d3cb934cfdb4fd68077d5d9
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-10 17:52:09 +0900

    PTD-192 Feat : AuthMobileView 컴포넌트 개발
    AuthMobileView 컴포넌트 개발 : 휴대폰 인증 뷰 컴포넌트 개발

---
+++ components/auth/views/AuthMobileView.tsx
@@ -0,0 +1,92 @@
+import WTextField from '@components/common/inputs/textField';
+import WAlert from '@components/common/modals/WAlert';
+import { Box, Grid, Stack } from '@mui/material';
+import AuthTimer from '../inputs/AuthTimer';
+import { AuthButton, TermsGrid } from '../signup/styled';
+import { AuthMobileViewType } from '../types';
+
+const AuthMobileView = (props: AuthMobileViewType) => {
+  const {
+    open,
+    authValue,
+    mobileValue,
+    authDisabled,
+    numDisabled,
+    authError,
+    authOnChange,
+    mobileOnChange,
+    onTimerDisabled,
+    mobileError,
+    focusOutEvent,
+    onClickAuthNumSend,
+    signupAuthOnClick,
+    resetModalClose,
+    bgDisable,
+  } = props;
+
+  return (
+    <WAlert
+      open={open}
+      handleClose={resetModalClose}
+      maxWidth={'xl'}
+      handleEvent={signupAuthOnClick}
+      bgDisable={bgDisable}
+      disabled={numDisabled}
+      btnColor="#c1c1c1"
+      btnTitle="다음"
+      title="휴대폰번호 인증"
+      closeBtnOn
+    >
+      <Stack justifyContent="center" width={'720px'} alignItems="center">
+        <Stack width="387px">
+          {/* 휴대폰입력 */}
+          <TermsGrid sx={{ alignItems: 'flex-start' }}>
+            <Box sx={{ width: '257px' }}>
+              <WTextField
+                value={mobileValue}
+                onChange={mobileOnChange}
+                placeholder={'휴대폰번호를 입력'}
+                helper="숫자 11자리를 입력해 주세요."
+                error={mobileError}
+              />
+            </Box>
+            <Box sx={{ width: '120px', height: '48px' }}>
+              <AuthButton
+                disabled={mobileValue.length >= 12 ? false : true}
+                onClick={onClickAuthNumSend}
+              >
+                인증번호 발송
+              </AuthButton>
+            </Box>
+          </TermsGrid>
+          {/*  인증번호 */}
+          <WTextField
+            error={authError}
+            value={authValue}
+            onChange={authOnChange}
+            disabled={authDisabled}
+            placeholder={'인증번호'}
+            helper="숫자만 입력해주세요"
+            focusOutEvent={focusOutEvent}
+          />
+          <Grid container justifyContent={'flex-end'}>
+            <Box sx={{ height: '20px' }}>
+              {!authDisabled ? (
+                <AuthTimer
+                  time={3}
+                  action={onTimerDisabled}
+                  resend={() => onClickAuthNumSend()}
+                />
+              ) : (
+                ''
+              )}
+            </Box>
+          </Grid>
+          <Box sx={{ height: '136px' }} />
+        </Stack>
+      </Stack>
+    </WAlert>
+  );
+};
+
+export default AuthMobileView;
