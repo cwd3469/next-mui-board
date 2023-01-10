9982ecf5a63d673880224a7a6bf766e73b2471fc
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-10 17:41:51 +0900

    PTD-192 Feat : WPwTextField 컴포넌트 개발
    WPwTextField 컴포넌트 개발 : 비밀번호 입력 컴포넌트 개발

---
+++ components/common/inputs/textField/modules/WPwTextField.tsx
@@ -0,0 +1,77 @@
+import React, { useCallback, useState } from 'react';
+import { WTextFieldModulesType } from '../type';
+import useValidation from '@hooks/utils/useValidation';
+import WTextField from '../index';
+
+const WPwTextField = (props: WTextFieldModulesType) => {
+  const { state, setState, keyId, err, setErr, disabled } = props;
+  const stateTxt = state as string;
+  const valid = useValidation();
+  const errMsg = useCallback(() => {
+    setErr(
+      {
+        msg: '조건에 맞는 비밀번호를 입력해 주세요.',
+        boo: true,
+      },
+      keyId,
+    );
+  }, [keyId, setErr]);
+  const passMsg = useCallback(() => {
+    setErr(
+      {
+        msg: '',
+        boo: false,
+      },
+      keyId,
+    );
+  }, [keyId, setErr]);
+
+  const onFocusOut = useCallback(
+    (value: string) => {
+      if (valid.regPwFormChack.test(value)) {
+        passMsg();
+      } else {
+        errMsg();
+      }
+    },
+    [errMsg, passMsg, valid.regPwFormChack],
+  );
+
+  const onChangeInfo = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      if (value.length <= 16) {
+        if (valid.regExpPass.test(value)) {
+          setState(value, keyId);
+          onFocusOut(value);
+        } else {
+          errMsg();
+        }
+      }
+    },
+    [errMsg, keyId, onFocusOut, setState, valid.regExpPass],
+  );
+
+  return (
+    <WTextField
+      type="password"
+      value={stateTxt}
+      onChange={onChangeInfo}
+      focusInEvent={passMsg}
+      focusOutEvent={() => onFocusOut(stateTxt)}
+      disabled={disabled}
+      error={err}
+      maxRows={16}
+      placeholder="비밀번호를 입력해 주세요."
+      helper={
+        '최소 8자리 이상 영어 대문자, 소문자, 숫자, 특수문자 중 3종류이상 포함해주세요.'
+      }
+    />
+  );
+};
+
+WPwTextField.defaultProps = {
+  disabled: false,
+};
+
+export default WPwTextField;
