42b71d8acd1916d95e2fb60b104737dfd91741a5
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-10 17:37:36 +0900

    PTD-192 Feat :SignupStepTwo 컴포넌트 개발
    SignupStepTwo 컴포넌트 개발 : 회원가입 휴대폰인증 모달 개발

---
+++ components/auth/signup/modal/SignupStepTwo.tsx
@@ -0,0 +1,106 @@
+import useMobileAuth from '@components/auth/hooks/useMobileAuth';
+import AuthMobileView from '@components/auth/views/AuthMobileView';
+import { ModalType } from '@components/common/layouts/gnb/types';
+import { useToastContext } from '@hooks/utils/useToastContext';
+import { useCallback, useState } from 'react';
+
+const SignupStepTwo = (props: ModalType) => {
+  const {
+    authValue,
+    mobileValue,
+    authDisabled,
+    numDisabled,
+    authError,
+    mobileError,
+    onAbledAuthInput,
+    onChangeAuthNum,
+    onChangeMobile,
+    onClickReset,
+    onFocusOutAuthNum,
+    onTimerDisabled,
+    onSetAuthDisabled,
+  } = useMobileAuth();
+  //   const toast = useToastContext();
+  const [bgDisable, setBgDisable] = useState<boolean>(false);
+  const [modalOn, setModalOn] = useState<boolean>(false);
+  //   const { mutate: postCommonMobileAuthMutate } = useCommonMobileAuth();
+  //   const { mutate: postCommonVerifyCodeMutate } = useCommonVerifyCode();
+
+  const resetModalClose = () => {
+    onClickReset();
+    props.handleClose();
+    setBgDisable(false);
+    setModalOn(false);
+  };
+
+  const onClickAuthNumSend = useCallback(() => {
+    onAbledAuthInput();
+    // const mobile: MobileNumDto = { mobileNum: mobileFormatOff(mobileValue) };
+    // postCommonMobileAuthMutate(mobile, {
+    //   onSuccess: (res) => {
+    //     if (res.data.code !== '0000') {
+    //       toast?.on(msg.errMsg(res.data.code),'error');
+    //       return;
+    //     }
+    //     onAbledAuthInput();
+    //   },
+    //   onError: (err) => {
+    //     toast?.on(
+    //       '인증번호 발급에 실패 하였습니다. \n 잠시 후, 다시 시도해 주세요.',
+    //     ,'error');
+    //   },
+    // });
+  }, [onAbledAuthInput]);
+
+  const signupAuthOnClick = useCallback(() => {
+    setModalOn(true);
+    setBgDisable(true);
+    onSetAuthDisabled();
+    // const data = {
+    //   mobileNum: mobileFormatOff(mobileValue),
+    //   code: authValue,
+    // };
+    // postCommonVerifyCodeMutate(data, {
+    //   onSuccess: (res) => {
+    //     if (res.data.status === 'SUCCESS') {
+    //       setModalOn(true);
+    //       setAuthDisabled(true);
+    //       setBgDisable(true);
+    //     }
+    //     if (res.data.status === 'FAIL') {
+    //       toast?.on(msg.errMsg(res.data.code) , 'error');
+    //       return;
+    //     }
+    //   },
+    //   onError: (err) => {
+    //     toast?.on(
+    //       '인증번호가 일치하지 않습니다 \n 잠시 후, 다시 시도해 주세요.',
+    //     , 'error');
+    //   },
+    // });
+  }, [onSetAuthDisabled]);
+
+  return (
+    <>
+      <AuthMobileView
+        open={props.open}
+        authValue={authValue}
+        mobileValue={mobileValue}
+        authDisabled={authDisabled}
+        bgDisable={bgDisable}
+        numDisabled={numDisabled}
+        authError={authError}
+        mobileError={mobileError}
+        authOnChange={onChangeAuthNum}
+        mobileOnChange={onChangeMobile}
+        onTimerDisabled={onTimerDisabled}
+        focusOutEvent={onFocusOutAuthNum}
+        onClickAuthNumSend={onClickAuthNumSend}
+        signupAuthOnClick={signupAuthOnClick}
+        resetModalClose={resetModalClose}
+      />
+    </>
+  );
+};
+
+export default SignupStepTwo;
