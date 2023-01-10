8a622cfc8a55b9ba99388dbbaa2e2747d533ce29
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-10 17:39:21 +0900

    PTD-192 Feat : WAddressDetailTextField 컴포넌트 개발
    WAddressDetailTextField 컴포넌트 개발 : 주소 상세 페이지 컴포넌트 개발

---
+++ components/common/inputs/textField/modules/WAddressDetailTextField.tsx
@@ -0,0 +1,62 @@
+import React, { useCallback, useState } from 'react';
+import { WTextFieldModulesType } from '../type';
+import useValidation from '@hooks/utils/useValidation';
+import WTextField from '../index';
+
+const WAddressDetailTextField = (props: WTextFieldModulesType) => {
+  const { state, setState, keyId, err, setErr, disabled } = props;
+  const stateTxt = state as string;
+  const valid = useValidation();
+  const errMsg = useCallback(() => {
+    setErr(
+      {
+        msg: '조건에 맞는 약국명을 입력해 주세요.',
+        boo: true,
+      },
+      keyId,
+    );
+  }, [keyId, setErr]);
+  const passMsg = useCallback(() => {
+    setErr(
+      {
+        msg: '',
+        boo: false,
+      },
+      keyId,
+    );
+  }, [keyId, setErr]);
+
+  const onChangeInfo = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      if (value.length <= 15) {
+        if (valid.regKoEnNum.test(value)) {
+          setState(value, keyId);
+
+          passMsg();
+        } else {
+          errMsg();
+        }
+      }
+    },
+    [errMsg, keyId, passMsg, setState, valid.regKoEnNum],
+  );
+
+  return (
+    <WTextField
+      value={stateTxt}
+      onChange={onChangeInfo}
+      error={err}
+      focusInEvent={passMsg}
+      disabled={disabled}
+      placeholder="약국상세 주소를 입력 해주세요."
+      helper={'최대 15자리까지 입력 가능합니다.'}
+    />
+  );
+};
+
+WAddressDetailTextField.defaultProps = {
+  disabled: false,
+};
+
+export default WAddressDetailTextField;
