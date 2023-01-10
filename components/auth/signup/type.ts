12025672ab56525c867789ab798c8842c5dcd9c8
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-10 17:33:34 +0900

    PTD-192 Feat : signup / type 개발
    signup / type 개발 : 회원가입 타입 정리

---
+++ components/auth/signup/type.ts
@@ -0,0 +1,50 @@
+import { ErrorType } from '@components/common/inputs/type';
+import { ModalType } from '@components/common/layouts/gnb/types';
+
+export interface BusiNumber {
+  oneNum: string;
+  twoNum: string;
+  threeNum: string;
+}
+
+export interface SignupStepThreeType extends ModalType {
+  mobileValue: string;
+}
+
+export interface SignupFileUlid {
+  bizCertFileUlid: string;
+  telecomUseCertFileUlid: string;
+  passbookCopyFileUlid: string;
+}
+
+export interface SignupInfo {
+  [key: string]: string;
+  id: string;
+  pw: string;
+  rePw: string;
+  addressDetail: string;
+  name: string;
+  buisNum: string;
+  postCode: string;
+  pharmacyName: string;
+  pharmacyAddress: string;
+  pharmacyPhone: string;
+  pharmacyTaxNum: string;
+  businessLicense: string;
+  comnetUseCert: string;
+  bankbookCopy: string;
+}
+
+export interface SignupInfoError {
+  [key: string]: ErrorType;
+  idErr: ErrorType;
+  pwErr: ErrorType;
+  rePwErr: ErrorType;
+  addressDetailErr: ErrorType;
+  nameErr: ErrorType;
+  buisNumErr: ErrorType;
+  pharmacyNameErr: ErrorType;
+  pharmacyAddressErr: ErrorType;
+  pharmacyPhoneErr: ErrorType;
+  pharmacyTaxNumErr: ErrorType;
+}
