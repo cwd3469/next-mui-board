e3053247be538bdeb7309e25e68a873747b18347
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-10 17:52:42 +0900

    PTD-192 Feat : SignupTerms 컴포넌트 개발
    SignupTerms 컴포넌트 개발 : 약관 확인 모달 컴포넌트 개발

---
+++ components/auth/signup/modal/SignupTerms.tsx
@@ -0,0 +1,32 @@
+import { Box, Stack, Typography } from '@mui/material';
+import WAlert from '@components/common/modals/WAlert';
+import { ModalType } from '@components/common/layouts/gnb/types';
+
+const SignupTerms = (props: ModalType) => {
+  const { open, handleClose } = props;
+
+  return (
+    <WAlert
+      open={open}
+      handleClose={handleClose}
+      maxWidth="xl"
+      btnColor="#c1c1c1"
+      btnTextColor="#999"
+      title=" 서비스 이용약관 동의"
+    >
+      <Stack alignItems={'center'} padding="0px 40px">
+        <Box
+          width={'640px'}
+          height={'420px'}
+          sx={{ overflowY: 'scroll' }}
+          padding={'10px'}
+        >
+          <Box>서비스 이용약관 동의 전체 내용입니다.</Box>
+        </Box>
+        <Box height="41px" />
+      </Stack>
+    </WAlert>
+  );
+};
+
+export default SignupTerms;
