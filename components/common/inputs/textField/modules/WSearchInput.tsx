94249ceeb97acdd2be406c622d5f133e3251a9f4
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-12 20:50:09 +0900

    PTD-194 Feat : WSearchInput 컴포넌트 개발
    WSearchInput 컴포넌트 개발 : 검색 텍스트 필드 컴포넌트 개발

---
+++ components/common/inputs/textField/modules/WSearchInput.tsx
@@ -0,0 +1,73 @@
+import Image from 'next/image';
+import { Box, IconButton, styled, SxProps, TextField } from '@mui/material';
+import sreach from 'public/assets/icon/search.svg';
+import { useState } from 'react';
+import { Theme } from '@mui/system';
+
+export const SearchTextField = styled(TextField)(({ theme }) => ({
+  width: '276px',
+  backgroundColor: '#fff',
+  ...theme.typography.body1,
+  borderRadius: '6px',
+  '& .MuiInputBase-input': {
+    border: '1px solid #e0e1e2',
+    padding: '10px 40px 10px 10px',
+    color: '#000',
+    borderRadius: '6px',
+  },
+  '& .MuiOutlinedInput-notchedOutline': {
+    borderColor: '#E0E1E2',
+    borderRadius: '6px',
+  },
+}));
+
+const WSearchInput = (props: {
+  search: (txt: string) => void;
+  placeholder: string;
+  sx?: SxProps<Theme>;
+}) => {
+  const { search, placeholder, sx } = props;
+  const [value, setValue] = useState<string>('');
+  const searchEvent = () => {
+    search(value);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'Enter') {
+      searchEvent();
+    }
+  };
+
+  return (
+    <Box position="relative">
+      <SearchTextField
+        placeholder={placeholder}
+        onKeyDown={handleKeyPress}
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+        sx={sx}
+      />
+      <IconButton
+        sx={{
+          position: 'absolute',
+          right: '3px',
+          top: '50%',
+          transform: 'translateY(-50%)',
+        }}
+        onClick={searchEvent}
+      >
+        <Box
+          sx={{
+            width: '18px',
+            height: '18px',
+            position: 'relative',
+          }}
+        >
+          <Image src={sreach} alt="검색아이콘" layout="fill" />
+        </Box>
+      </IconButton>
+    </Box>
+  );
+};
+
+export default WSearchInput;
