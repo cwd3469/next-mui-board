20473f3ea013eca8934c735d7a8c78943652fbfa
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-12 22:05:53 +0900

    PTD-194 Feat : transtext 파일 개발
    transtext 파일 개발

---
+++ utils/transtext.ts
@@ -0,0 +1,68 @@
+import { FilterAllOtions, FilterDateType } from '@hooks/contexts/filters/type';
+
+export const sideDot = (txt: string, spe: string) => {
+  const center = txt.substring(4);
+  const dot = `${spe}${spe}${spe}${spe}${center}`;
+  return dot;
+};
+
+export const transTextStateTreat = (state: string) => {
+  switch (state) {
+    case 'HOLD':
+      return '진료 보류';
+    case 'WAIT':
+      return '진료 대기';
+    case 'CLOSE':
+      return '진료 종료';
+    case 'CANCEL':
+      return '진료 취소';
+    default:
+      return '진료 중';
+  }
+};
+
+export const transTextStateReception = (state: string) => {
+  switch (state) {
+    case 'ACCEPT':
+      return '접수 수락';
+    case 'REFUSE':
+      return '접수 거절';
+    default:
+      return '접수 대기';
+  }
+};
+
+export const transQueryUrl = (filter: FilterAllOtions) => {
+  const page = filter.page ? `&page=${filter.page - 1}` : '';
+  const code = filter.code === '0' ? '' : `&code=${filter.code}`;
+  const nameKo = filter.nameKo ? `&nameKo=${filter.nameKo}` : '';
+  const location = filter.location
+    ? filter.location !== 'DEFULT'
+      ? `&location=${filter.location}`
+      : ''
+    : '';
+  const status = filter.status
+    ? filter.status !== 'DEFULT'
+      ? `&status=${filter.status}`
+      : ''
+    : '';
+  const enterType = filter.enterType
+    ? filter.enterType !== 'DEFULT'
+      ? `&enterType=${filter.enterType}`
+      : ''
+    : '';
+  const isDone = filter.isDone
+    ? filter.enterType !== 'DEFULT'
+      ? `&isDone=${filter.isDone}`
+      : ''
+    : '';
+
+  return `${page}${code}${nameKo}${location}${status}${enterType}`;
+};
+
+export const transQueryDate = (filter: FilterDateType) => {
+  const startDate = filter.startDate ? `&startDate=${filter.startDate}` : '';
+  const endDate = filter.endDate ? `&endDate=${filter.endDate}` : '';
+
+  return `${endDate}${startDate}`;
+};
