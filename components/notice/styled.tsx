eeba722b57ce06731b452000b211f56f211c154f
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-13 14:05:23 +0900

    PTD-194 Feat : notice > styled 컴포넌트 개발
    notice > styled 컴포넌트 개발 : 공지 사항 스타일 컴포넌트 정리

---
+++ components/notice/styled.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+import { Box } from '@mui/material';
+
+export const WNotionBoardHead = styled(Box)(({ theme }) => ({
+  padding: '30px 38px',
+  borderBottom: '1px solid #ebeced',
+  height: '108px',
+  '& .title': {
+    fontWeight: '400',
+    lineHeight: '1',
+  },
+  '& .info': {
+    lineHeight: '1',
+  },
+  '& .info.name': {
+    color: '#333',
+  },
+  '& .info.substance': {
+    color: '#555',
+  },
+}));
+export const WNotionBoardBody = styled(Box)(({ theme }) => ({
+  padding: '30px 35px',
+  overflowY: 'scroll',
+  overflowX: 'hidden',
+  height: '572px',
+}));
