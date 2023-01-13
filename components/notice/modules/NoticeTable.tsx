66bd2e8f8e837ba1c0595fbe0e22f62e59b97677
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-13 14:03:19 +0900

    PTD-194 Refactor : NoticeTable 리팩토링
    NoticeTable 리팩토링 : 공지 상세 페이지 이동 추가

--- components/notice/modules/NoticeTable.tsx
+++ components/notice/modules/NoticeTable.tsx
@@ -1,38 +1,64 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { GridColDef } from '@mui/x-data-grid';
 import { NoticeInterface } from '../type';
 import WDataTable, { baseOption } from '@components/common/dataGrid/WDataTable';
+import { Button } from '@mui/material';
+import { useRouter } from 'next/router';
 
 const NoticeTable = (props: { data: NoticeInterface[] }): JSX.Element => {
   const { data } = props;
   const rows = data.map((item, index) => {
     return { ...item, ['id']: index };
   });
-
+  const router = useRouter();
+  const onNoticeDetail = useCallback(
+    (id: string) => {
+      router.push({
+        pathname: `/notice/${id}`,
+      });
+    },
+    [router],
+  );
   const columns: GridColDef[] = [
     {
+      ...baseOption,
       field: 'number',
       headerName: '번호',
       width: 200,
-      ...baseOption,
     },
     {
+      ...baseOption,
       field: 'status',
       headerName: '구분',
       width: 200,
-      ...baseOption,
     },
     {
+      ...baseOption,
       field: 'title',
       headerName: '제목',
       width: 600,
-      ...baseOption,
+      renderCell(params) {
+        return (
+          <Button
+            sx={{
+              width: '100%',
+              height: '62px',
+              color: '#555',
+              fontWeight: '400',
+            }}
+            variant="text"
+            onClick={() => onNoticeDetail(params.row.ulid)}
+          >
+            {params.row.title}
+          </Button>
+        );
+      },
     },
     {
+      ...baseOption,
       field: 'createAt',
       headerName: '작성일',
       width: 190,
-      ...baseOption,
     },
   ];
 
