5c234c345e141fece6327ecd5fdd16ffbf5a9337
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-13 13:52:24 +0900

    PTD-194 Refactor : WButton 디자인 추가
    WButton 디자인 추가 : hover boxshadow 삭제

--- components/common/button/WButton.tsx
+++ components/common/button/WButton.tsx
@@ -1,8 +1,15 @@
-import { Button, styled } from '@mui/material';
+import { Button, ButtonProps, styled } from '@mui/material';
 
-export const WButton = styled(Button)(({ theme }) => ({
+export const WButton = styled((props: ButtonProps) => {
+  return <Button {...props}>{props.children}</Button>;
+})(({ theme }) => ({
   width: '180px',
   height: '60px',
   boxShadow: 'none',
   borderRadius: '6px',
+  fontSize: '16px',
+  letterSpacing: '0px',
+  '&:hover': {
+    boxShadow: '0px 0px 0px',
+  },
 }));
