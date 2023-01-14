150201e72e31164f8134e523537fd44ad909a7c9
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-14 18:48:51 +0900

    PTD-296 Refactor : 영업시간 설정 페이지 리팩토링
    영업시간 설정 페이지 리팩토링 :

--- pages/setting/business-hours.tsx
+++ pages/setting/business-hours.tsx
@@ -1,18 +1,15 @@
 import { Gnb } from '@components/common/layouts/gnb/Gnb';
 import { WLayout } from '@components/common/layouts/WLayout';
 import BusinessHourPage from '@components/setting/businessHour/BusinessHourPage';
-import { NoticeFilterProvider } from '@hooks/contexts/filters/NoticeFilterContext';
 
 const BusinessHour = () => {
   return (
-    <NoticeFilterProvider>
-      <div>
-        <Gnb />
-        <WLayout>
-          <BusinessHourPage />
-        </WLayout>
-      </div>
-    </NoticeFilterProvider>
+    <div>
+      <Gnb />
+      <WLayout>
+        <BusinessHourPage />
+      </WLayout>
+    </div>
   );
 };
 
