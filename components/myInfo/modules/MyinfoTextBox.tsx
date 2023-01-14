65885bb24da9d8d596568f5a2d5476caea1f6c0a
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-14 20:49:05 +0900

    PTD-283 Feat : MyInfoTextBox 컴포넌트 개발
    MyInfoTextBox 컴포넌트 개발 : 내 정보 리뷰 컴포넌트 개발

---
+++ components/myInfo/modules/MyinfoTextBox.tsx
@@ -0,0 +1,27 @@
+import { BoxDisabledText } from '@components/common/box/modules/TextBox';
+import WSubTitle from '@components/common/typography/WSubTitle';
+import { SxProps, Theme } from '@mui/material';
+import { FlexCenterC, FlexCenterR, FlexStartC } from '@styles/flexGrid';
+
+const MyInfoTextBox = (props: {
+  title: string;
+  children: string | JSX.Element;
+  sx?: SxProps<Theme>;
+}) => {
+  const { title, children, sx } = props;
+  return (
+    <FlexStartC width="370px" gap="16px" sx={sx}>
+      <WSubTitle
+        title={title}
+        sx={{
+          '& .MuiTypography-root': {
+            lineHeight: '24px',
+          },
+        }}
+      />
+      <BoxDisabledText>{children}</BoxDisabledText>
+    </FlexStartC>
+  );
+};
+
+export default MyInfoTextBox;
