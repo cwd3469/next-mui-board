8b18daaaa24afa6b34bd514101d14fb1c330280f
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-19 14:24:37 +0900

    PTD-204 Fix : 조제 내역 페이지 더미데이터 수정
    조제 내역 페이지 더미데이터 수정

--- components/preparation/history/HistoryPage.tsx
+++ components/preparation/history/HistoryPage.tsx
@@ -1,25 +1,23 @@
 import WPagination from '@components/common/button/WPagination';
-import useNoticeList from '@hooks/apis/notion/hooks/useNotice';
-import { NoticeFilterContext } from '@hooks/contexts/filters/NoticeFilterContext';
-import useCodeMsgBundle from '@hooks/utils/useCodeMsgBundle';
-import { useToastContext } from '@hooks/utils/useToastContext';
+import useListHistory from '@hooks/apis/preparation/history/hooks/useListHistory';
+import { HistoryFilterContext } from '@hooks/contexts/filters/HistoryFilterContext';
 import { Stack } from '@mui/material';
 import { useContext, useEffect } from 'react';
 import HistoryFilter from './modules/HistoryFilter';
 import HistoryTable from './modules/HistoryTable';
-import { HistoryInterface } from './type';
 
 const HistoryPage = () => {
-  const { filter, setInFilter } = useContext(NoticeFilterContext);
-  const { noticeData, totalPages } = useNoticeList(filter);
+  const { filter, date, setInFilter } = useContext(HistoryFilterContext);
+  const { historyListData, totalPages } = useListHistory({ filter, date });
 
   const pagination = (event: React.ChangeEvent<unknown>, value: number) => {
     setInFilter(value, 'page');
   };
+
   return (
     <Stack gap="20px">
       <HistoryFilter />
-      <HistoryTable data={noticeData} />
+      <HistoryTable data={historyListData} />
       <WPagination
         paddingTop="4px"
         page={filter.page}
