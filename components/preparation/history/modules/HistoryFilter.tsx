e522b7c17192cb1356144c04df254e2b0fa11ce1
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-19 14:09:00 +0900

    PTD-204 Feat : HistoryFilter 컴포넌트 개발
    HistoryFilter 컴포넌트 개발 : 조제 내역 필터 컴포넌트 개발

---
+++ components/preparation/history/modules/HistoryFilter.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import WSearchInput from '@components/common/inputs/textField/modules/WSearchInput';
+import WSelectNotice from '@components/common/inputs/select/modules/WSelectNotice';
+import { HistoryFilterContext } from '@hooks/contexts/filters/HistoryFilterContext';
+import WSelectPreparation from '@components/common/inputs/select/modules/WSelectPreparation';
+import WDatePicker from '@components/common/inputs/datepicker';
+import WDatePickerFilter from '@components/common/inputs/datepicker/modules/WDatePickerFilter';
+import WSelectDelivery from '@components/common/inputs/select/modules/WSelectDelivery';
+
+export interface OptionType {
+  id: string;
+  name: string;
+}
+
+const HistoryFilter = () => {
+  const { setInFilter, setInDate } = React.useContext(HistoryFilterContext);
+
+  const searchEvent = (txt: string) => {
+    setInFilter(txt, 'keyword');
+  };
+
+  return (
+    <Grid container alignItems={'center'} justifyContent={'space-between'}>
+      <Grid container width="auto" gap={'10px'}>
+        <WDatePickerFilter setInDate={setInDate} />
+        <WSelectPreparation
+          callBack={(id: string, keyId: string) => setInFilter(id, keyId)}
+        />
+        <WSelectDelivery
+          callBack={(id: string, keyId: string) => setInFilter(id, keyId)}
+        />
+        <WSearchInput search={searchEvent} placeholder="제목 검색" />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default HistoryFilter;
