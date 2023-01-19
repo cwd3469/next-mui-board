6449f8b7ea99a6e0d1b9bae8e227ea30b0bbedf9
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-19 14:07:46 +0900

    PTD-204 Feat : 조제 내역 페이지 타입 정리
    조제 내역 페이지 타입 정리

---
+++ components/preparation/history/type.ts
@@ -0,0 +1,14 @@
+export interface HistoryInterface {
+  ulid: string;
+  status: string;
+  statusNameKo: string;
+  deliveryStatus: string;
+  deliveryStatusKo: string;
+  deliveryNumber: string;
+  completionAt: string;
+  refuseReason: string;
+  receive: string;
+  treatHospitalName: string;
+  treatDoctorName: string;
+  treatHospitalPhone: string;
+}
