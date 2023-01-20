b093b218b0e06a48b3eba3844bf6384afb6bff01
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-20 21:20:47 +0900

    PTD-202 Refactor : useAxiosInterceptor 토큰 리프레쉬 적용
    useAxiosInterceptor 토큰 리프레쉬 적용

--- hooks/apis/useAxiosInterceptor.ts
+++ hooks/apis/useAxiosInterceptor.ts
@@ -1,12 +1,18 @@
+import useCodeMsgBundle from '@hooks/utils/useCodeMsgBundle';
 import { useToastContext } from '@hooks/utils/useToastContext';
+import { deleteCookie, getCookie, setCookie } from 'cookies-next';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { useMutation } from 'react-query';
+import { apiTokenValidation } from './auth/common';
 import instance from './instance';
 
 const useAxiosInterceptor = () => {
   const [progressOn, setProgressOn] = useState<boolean>(false);
+  const { mutate: mutateTokenValidation } = useMutation(apiTokenValidation);
   const router = useRouter();
   const toast = useToastContext();
+  const msg = useCodeMsgBundle();
   useEffect(() => {
     const interceptorReq = instance.interceptors.request.use(
       (config) => {
@@ -26,6 +32,29 @@ const useAxiosInterceptor = () => {
       },
       (error) => {
         console.log(error);
+        const status = error.response.status;
+        if (status === 498) {
+          const token = getCookie('refreshToken');
+          const refreshToken = typeof token === 'string' ? token : '';
+          mutateTokenValidation(refreshToken, {
+            onSuccess(data, variables, context) {
+              if (data.data.code) {
+                if (data.data.code !== '0000') {
+                  router.replace('/signin', undefined, {
+                    shallow: true,
+                  });
+                  deleteCookie('accessToken');
+                  deleteCookie('refreshToken');
+                  toast?.on(msg.errMsg(data.data.code), 'error');
+                  return;
+                }
+              }
+              setCookie('accessToken', data.data.accessToken);
+              setCookie('refreshToken', data.data.refreshToken);
+              return instance(error.config);
+            },
+          });
+        }
 
         setProgressOn(false);
         return Promise.reject(error);
