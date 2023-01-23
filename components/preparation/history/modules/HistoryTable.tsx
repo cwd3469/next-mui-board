732821259c34b59b91a1136ef321922d037ff9e7
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-23 13:22:21 +0900

    PTD-202 Feat : HistoryTable 기능 추가
    HistoryTable 기능 추가 : 요청자 정보 모달 노출 기능 추가

--- components/preparation/history/modules/HistoryTable.tsx
+++ components/preparation/history/modules/HistoryTable.tsx
@@ -1,18 +1,45 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { GridColDef } from '@mui/x-data-grid';
 import { HistoryInterface } from '../type';
 import WDataTable, { baseOption } from '@components/common/dataGrid/WDataTable';
-import { Button, Typography } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
 import { dateFormat } from '@utils/date';
 import GridButton from '@components/common/button/modules/GridButton';
+import {
+  CheckIcon,
+  TruckIcon,
+  UserIcon,
+} from '@components/common/dataDisplay/WIcons';
+import PreparationRequesterModal from '@components/preparation/modals/PreparationRequesterModal';
 
 const HistoryTable = (props: { data: HistoryInterface[] }): JSX.Element => {
   const { data } = props;
+  const [requesterId, setRequesterId] = useState<string>('');
+  const [requesterOpen, setRequesterOpen] = useState<boolean>(false);
+  const [prescriptionId, setPrescriptionId] = useState<string>('');
+  const [prescriptionOpen, setPrescriptionOpen] = useState<boolean>(false);
+  const [deliveryId, setDeliveryId] = useState<string>('');
+  const [deliveryOpen, setDeliveryOpen] = useState<boolean>(false);
+
   const rows = data.map((item, index) => {
     return { ...item, ['id']: index };
   });
   const router = useRouter();
+
+  const requesterOnOff = useCallback((id: string, open: boolean) => {
+    setRequesterId(id);
+    setRequesterOpen(open);
+  }, []);
+  const prescriptionIdOnOff = useCallback((id: string, open: boolean) => {
+    setPrescriptionId(id);
+    setPrescriptionOpen(open);
+  }, []);
+  const deliveryIdOnOff = useCallback((id: string, open: boolean) => {
+    setDeliveryId(id);
+    setDeliveryOpen(open);
+  }, []);
+
   const onNoticeDetail = useCallback(
     (id: string) => {
       router.push({
@@ -54,7 +81,7 @@ const HistoryTable = (props: { data: HistoryInterface[] }): JSX.Element => {
     },
     {
       ...baseOption,
-      field: 'receive',
+      field: 'deliveryFormKo',
       headerName: '수령 방법',
       width: 70,
     },
@@ -63,14 +90,49 @@ const HistoryTable = (props: { data: HistoryInterface[] }): JSX.Element => {
       field: 'deliveryStatusKo',
       headerName: '배송 상태',
       width: 130,
+      renderCell: (prams) => {
+        const {
+          deliveryStatus,
+          deliveryStatusKo,
+          waybillNumber,
+          courier,
+          deliveryForm,
+        } = prams.row;
+        return deliveryForm !== 'DELIVERY' ? (
+          deliveryStatusKo
+        ) : deliveryStatus === 'WAIT' ? (
+          deliveryStatusKo
+        ) : (
+          <Stack justifyContent={'center'} width="100%">
+            <Typography textAlign={'center'}>{courier}</Typography>
+            <Typography textAlign={'center'}>{waybillNumber}</Typography>
+          </Stack>
+        );
+      },
     },
     {
       ...baseOption,
       field: 'requesterInfo',
       headerName: '요청자 정보',
       width: 120,
-      renderCell: () => {
-        return <GridButton>요청자 정보</GridButton>;
+      renderCell: (prams) => {
+        const { deliveryStatus, status, ulid } = prams.row;
+
+        return (
+          <GridButton
+            onClick={() => requesterOnOff(ulid, true)}
+            startIcon={<UserIcon />}
+            disabled={
+              status === 'CANCEL'
+                ? true
+                : deliveryStatus === 'COMPLETION'
+                ? true
+                : false
+            }
+          >
+            요청자 정보
+          </GridButton>
+        );
       },
     },
     {
@@ -78,11 +140,13 @@ const HistoryTable = (props: { data: HistoryInterface[] }): JSX.Element => {
       field: 'treatHospitalName',
       headerName: '진료 병원 명',
       width: 125,
-      renderCell: (params) => {
+      renderCell: (prams) => {
         return (
-          <Typography textAlign="center">
-            {params.row.refuseReason ? params.row.treatHospitalName : '-'}
-          </Typography>
+          <Stack justifyContent={'center'} width="100%">
+            <Typography textAlign={'center'}>
+              {prams.row.treatHospitalName}
+            </Typography>
+          </Stack>
         );
       },
     },
@@ -91,13 +155,6 @@ const HistoryTable = (props: { data: HistoryInterface[] }): JSX.Element => {
       field: 'treatDoctorName',
       headerName: '진료 병원 의사 명',
       width: 100,
-      renderCell: (params) => {
-        return (
-          <Typography textAlign="center">
-            {params.row.refuseReason ? params.row.treatDoctorName : '-'}
-          </Typography>
-        );
-      },
     },
     {
       ...baseOption,
@@ -110,8 +167,22 @@ const HistoryTable = (props: { data: HistoryInterface[] }): JSX.Element => {
       field: 'prescriptionPreview',
       headerName: '처방전 보기',
       width: 120,
-      renderCell: () => {
-        return <GridButton>처방전 보기</GridButton>;
+      renderCell: (prams) => {
+        const { status, deliveryStatus } = prams.row;
+        return (
+          <GridButton
+            startIcon={<CheckIcon />}
+            disabled={
+              status === 'CANCEL'
+                ? true
+                : deliveryStatus === 'WAIT'
+                ? false
+                : true
+            }
+          >
+            처방전 보기
+          </GridButton>
+        );
       },
     },
     {
@@ -119,13 +190,40 @@ const HistoryTable = (props: { data: HistoryInterface[] }): JSX.Element => {
       field: 'shippingRequest',
       headerName: '배송 요청',
       width: 120,
-      renderCell: () => {
-        return <GridButton>배송 요청</GridButton>;
+      renderCell: (prams) => {
+        const { status, deliveryStatus } = prams.row;
+        return (
+          <GridButton
+            startIcon={<TruckIcon />}
+            disabled={
+              status === 'CANCEL'
+                ? true
+                : deliveryStatus === 'WAIT'
+                ? false
+                : true
+            }
+          >
+            배송 요청
+          </GridButton>
+        );
       },
     },
   ];
 
-  return <WDataTable rows={rows} columns={columns} />;
+  return (
+    <>
+      <WDataTable rows={rows} columns={columns} />
+      {requesterId ? (
+        <PreparationRequesterModal
+          id={requesterId}
+          open={requesterOpen}
+          handleClose={() => requesterOnOff('', false)}
+        />
+      ) : (
+        ''
+      )}
+    </>
+  );
 };
 
 export default HistoryTable;
