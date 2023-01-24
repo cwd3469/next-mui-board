4bf2c420f07a3755b30a039c5da0a78979d1b25b
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-24 18:43:27 +0900

    PTD-202 Feat : 조제 진행 페이지 타입정의
    조제 진행 페이지 타입정의

---
+++ components/preparation/proceed/type.ts
@@ -0,0 +1,19 @@
+export interface ProceedInterface {
+  ulid: string;
+  status: string;
+  statusNameKo: string;
+  deliveryForm: string;
+  deliveryFormKo: string;
+  waybillNumber: string;
+  courier: string;
+  completionAt: string;
+  treatHospitalName: string;
+  treatDoctorName: string;
+  treatHospitalPhone: string;
+}
+
+export interface PreparationRequestDto {
+  name: string;
+  mobileNumber: string;
+  address: string;
+}
