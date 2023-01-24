9b2b42102eff44bd33df832bf4cbead3f8b107d2
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-24 18:38:37 +0900

    PTD-202 Feat : ProceedStatusNoti 컴포넌트 개발
    ProceedStatusNoti 컴포넌트 개발 : 조제 진행 알림 컴포넌트 개발

---
+++ components/preparation/proceed/modules/ProceedStatusNoti.tsx
@@ -0,0 +1,27 @@
+import WStatusNoti, {
+  WNotiBox,
+} from '@components/common/typography/WStatusNoti';
+import useQueryProceedyNoti from '@hooks/apis/preparation/proceed/hooks/useQueryProceedyNoti';
+import { Box, Grid } from '@mui/material';
+
+const ProceedStatusNoti = () => {
+  const { data } = useQueryProceedyNoti();
+
+  return (
+    <Grid container gap="16px" alignItems="center">
+      <WStatusNoti
+        counting={String(data.currentRequestsNumber)}
+        title="현제 조제 요청 건수"
+        units="건"
+      />
+      <WNotiBox />
+      <WStatusNoti
+        counting={String(data.currentPaymentsNumber)}
+        title="현제 결제 대기 건수"
+        units="건"
+      />
+    </Grid>
+  );
+};
+
+export default ProceedStatusNoti;
