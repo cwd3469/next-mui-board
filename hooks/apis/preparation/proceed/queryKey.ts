9d75a819925f46cc6932019251d48af7f9091dcd
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-24 18:41:37 +0900

    PTD-202 Feat : useListProceed 컴포넌트 개발
    useListProceed 컴포넌트 개발

---
+++ hooks/apis/preparation/proceed/queryKey.ts
@@ -0,0 +1,21 @@
+import {
+  FilterHistoryType,
+  FilterDateType,
+  FilterListData,
+} from '@hooks/contexts/filters/type';
+
+export const HISTORYLIST = (props: FilterListData) => [
+  'HISTORY',
+  'LIST',
+  { ...props.filter, ...props.date },
+];
+
+export const PREPARATIONREQUEST = (id: string) => [
+  'PREPARATION',
+  'REQUEST',
+  { id },
+];
+
+export const PRESCRIPTION = (id: string) => ['PRESCRIPTION', 'HISTORY', { id }];
+
+export const HISTORYSTATUS = () => ['HISTORY', 'STATUS'];
