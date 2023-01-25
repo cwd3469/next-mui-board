0282fb09120766f74bab7976d2ab92d33bb4ad5b
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-25 10:03:54 +0900

    PTD-202 Feat : WRefusalDispenTextField 컴포넌트 개발
    WRefusalDispenTextField 컴포넌트 개발 : 공통 거절 사유 입력 컴포넌트 개발

---
+++ components/common/inputs/textField/modules/WRefusalDispenTextField.tsx
@@ -0,0 +1,61 @@
+import React, { useCallback, useState } from 'react';
+import { WTextFieldModulesType } from '../type';
+import useValidation from '@hooks/utils/useValidation';
+import WTextField from '../index';
+
+const WRefusalDispenTextField = (props: WTextFieldModulesType) => {
+  const { state, setState, keyId, err, setErr, disabled } = props;
+  const stateTxt = state as string;
+  const valid = useValidation();
+  const errMsg = useCallback(() => {
+    setErr(
+      {
+        msg: '조제 거절 사유를 입력해 주세요.',
+        boo: true,
+      },
+      keyId,
+    );
+  }, [keyId, setErr]);
+  const passMsg = useCallback(() => {
+    setErr(
+      {
+        msg: '',
+        boo: false,
+      },
+      keyId,
+    );
+  }, [keyId, setErr]);
+
+  const onChangeInfo = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      if (value.length <= 20) {
+        if (valid.regRefusalDispensing.test(value)) {
+          setState(value, keyId);
+          passMsg();
+        } else {
+          errMsg();
+        }
+      }
+    },
+    [errMsg, keyId, passMsg, setState, valid.regRefusalDispensing],
+  );
+
+  return (
+    <WTextField
+      value={stateTxt}
+      onChange={onChangeInfo}
+      error={err}
+      focusInEvent={passMsg}
+      disabled={disabled}
+      placeholder="조제 거절 사유를 입력 해주세요."
+      helper={'20자 이내의 조제 거절 사유를 입력해 주세요.'}
+    />
+  );
+};
+
+WRefusalDispenTextField.defaultProps = {
+  disabled: false,
+};
+
+export default WRefusalDispenTextField;
