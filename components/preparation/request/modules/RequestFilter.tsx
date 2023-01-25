c4abdfad41915d07340ebfcf44019fa2a864ecb2
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-01-25 10:04:58 +0900

    PTD-202 Feat : RequestFilter 컴포넌트 개발
    RequestFilter 컴포넌트 개발 : 조제 요청 페이지 필터 컴포넌트 개발

---
+++ components/preparation/request/modules/RequestFilter.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import WSearchInput from '@components/common/inputs/textField/modules/WSearchInput';
+import WDatePickerFilter from '@components/common/inputs/datepicker/modules/WDatePickerFilter';
+import { RequestFilterContext } from '@hooks/contexts/filters/RequestFilterContext';
+
+export interface OptionType {
+  id: string;
+  name: string;
+}
+
+const RequestFilter = () => {
+  const { setInFilter, setInDate } = React.useContext(RequestFilterContext);
+
+  const searchEvent = (txt: string) => {
+    setInFilter(txt, 'keyword');
+  };
+
+  return (
+    <Grid container alignItems={'center'} justifyContent={'space-between'}>
+      <Grid container width="auto" gap={'10px'}>
+        <WDatePickerFilter setInDate={setInDate} />
+        <WSearchInput search={searchEvent} placeholder="제목 검색" />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default RequestFilter;
