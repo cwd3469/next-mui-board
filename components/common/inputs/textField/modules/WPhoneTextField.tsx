8de5a87c2b252970ebbe77073abf1f54ea081243
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-08 16:23:58 +0900

    PTSD-3 Feat : WPhoneTextField 리팩토링
    WPhoneTextField 리팩토링 : 유효성 검사 기능 수정

--- components/common/inputs/textField/modules/WPhoneTextField.tsx
+++ components/common/inputs/textField/modules/WPhoneTextField.tsx
@@ -4,73 +4,58 @@ import { WTextFieldModulesType } from '../type';
 import WTextField from '../index';
 
 const WPhoneTextField = (props: WTextFieldModulesType) => {
-  const { state, setState, keyId, err, setErr, disabled } = props;
-  const stateTxt = state as string;
+  const stateTxt = props.state as string;
   const valid = useValidation();
-
-  const onFocusInInfo = useCallback(() => {
-    const errMsg = {
-      msg: '',
-      boo: false,
-    };
-    setErr(errMsg, keyId);
-  }, [keyId, setErr]);
-
-  const errMsg = useCallback(() => {
-    setErr(
-      {
-        msg: '조건에 맞는 약국 전화번호를 입력해 주세요.',
-        boo: true,
-      },
-      keyId,
-    );
-  }, [keyId, setErr]);
-  const passMsg = useCallback(() => {
-    setErr(
-      {
-        msg: '',
-        boo: false,
-      },
-      keyId,
-    );
-  }, [keyId, setErr]);
 
   const onChangeInfo = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       const txt = e.target.value;
-      if (txt.length <= 20) {
-        if (txt.length !== 0 && !valid.regExpPhoneNumber.test(txt)) {
-          errMsg();
-          return;
-        }
-        if (txt.length <= 12) {
-          setState(
+      if (valid.regExpPhoneNumber.test(txt)) {
+        if (txt.length < 14) {
+          props.setState(
             txt.replace(/[^0-9]/g, '').replace(valid.regExpPhone, `$1-$2-$3`),
-            keyId,
+            props.keyId,
           );
-          passMsg();
+          const number = txt.replace('-', '').replace('-', '');
+          if (number.length < 8) {
+            props.setErr(
+              {
+                msg: '조건에 맞는 전화번호를 입력해 주세요.',
+                boo: true,
+              },
+              props.keyId,
+            );
+          } else {
+            props.setErr(
+              {
+                msg: '',
+                boo: false,
+              },
+              props.keyId,
+            );
+          }
         }
+      } else {
+        props.setErr(
+          {
+            msg: '조건에 맞는 전화번호를 입력해 주세요.',
+            boo: true,
+          },
+          props.keyId,
+        );
       }
     },
-    [
-      errMsg,
-      keyId,
-      passMsg,
-      setState,
-      valid.regExpPhone,
-      valid.regExpPhoneNumber,
-    ],
+    [props, valid.regExpPhone, valid.regExpPhoneNumber],
   );
 
   return (
     <WTextField
       value={stateTxt}
       onChange={onChangeInfo}
-      focusInEvent={onFocusInInfo}
       helper={'8~11자 이내의 약국 전화번호를 입력해 주세요.'}
       placeholder={'약국전화번호를 입력해 주세요.'}
-      disabled={disabled}
-      error={err}
+      disabled={props.disabled}
+      error={props.err}
     />
   );
 };
