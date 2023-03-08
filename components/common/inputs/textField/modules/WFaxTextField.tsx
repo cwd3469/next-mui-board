d70f623e4b4ad8f1fabf4bd50ad379a8ed1bc632
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-08 18:37:25 +0900

    PTSD-3 Refactor : WFaxTextField 리팩토링
    WFaxTextField 리팩토링 : focusInEvent 삭제

--- components/common/inputs/textField/modules/WFaxTextField.tsx
+++ components/common/inputs/textField/modules/WFaxTextField.tsx
@@ -8,14 +8,6 @@ const WFaxTextField = (props: WTextFieldModulesType) => {
   const stateTxt = state as string;
   const valid = useValidation();
 
-  const onFocusInInfo = useCallback(() => {
-    const errMsg = {
-      msg: '',
-      boo: false,
-    };
-    setErr(errMsg, keyId);
-  }, [keyId, setErr]);
-
   const errMsg = useCallback(() => {
     setErr(
       {
@@ -55,7 +47,6 @@ const WFaxTextField = (props: WTextFieldModulesType) => {
     <WTextField
       value={stateTxt}
       onChange={onChangeInfo}
-      focusInEvent={onFocusInInfo}
       helper={'숫자만 입력이 가능합니다.'}
       placeholder={'약국 팩스번호를 입력해 주세요.'}
       disabled={disabled}
