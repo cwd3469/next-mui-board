3b900b1692f1f87579d8d18df0a68a6c40f5bba2
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-09 09:15:28 +0900

    PTSD-3 Refactor : SigninLayout 리팩토링
    SigninLayout 리팩토링 : 디자인 수정

--- components/auth/styled.tsx
+++ components/auth/styled.tsx
@@ -1,9 +1,9 @@
 import { Box, styled } from '@mui/material';
 
 export const SigninLayout = styled(Box)(({ theme }) => ({
-  width: '528px',
+  width: '530px',
   backgroundColor: '#fff',
-  padding: '60px 104px 33px',
+  padding: '60px 75px ',
   borderRadius: '12px',
   position: 'absolute',
   top: '50%',
