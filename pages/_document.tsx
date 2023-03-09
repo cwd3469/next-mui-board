489836ced2249a2bfbac8696abed348a36713927
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-09 15:10:21 +0900

    PTSD-3 Feat : MyDocument 개발
    MyDocument 개발 : MyDocument.getInitialProps 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@mui/styles';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
 
-export default function Document() {
+export default function MyDocument() {
   return (
     <Html lang="en">
       <Head>
@@ -14,3 +16,25 @@ export default function Document() {
     </Html>
   );
 }
+
+MyDocument.getInitialProps = async (ctx: any) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) => (props: any) =>
+        sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
