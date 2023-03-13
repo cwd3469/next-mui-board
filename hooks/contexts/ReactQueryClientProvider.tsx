12a2a02ec679290655879bff957baffb332fa78b
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-13 18:43:41 +0900

    PTSD-14 Feat : ReactQueryClientProvider 개발
    ReactQueryClientProvider 개발 : QueryClientProvider custom hook 개발

---
+++ hooks/contexts/ReactQueryClientProvider.tsx
@@ -0,0 +1,64 @@
+import { useRouter } from 'next/router';
+import { useContext, useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { userInfoContext } from './userInfoContext';
+
+const ReactQueryClientProvider = (props: {
+  children: JSX.Element | JSX.Element[];
+}) => {
+  const router = useRouter();
+  const { handleTokenInfo } = useContext(userInfoContext);
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            cacheTime: 1800000,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            refetchInterval: 1800000,
+            retry: 1,
+            onSuccess(data: any) {
+              if (router.pathname !== '/signin') {
+                if (
+                  data.config.url !==
+                  '/telemedicine/api/v1/telemedicine-clinic/status'
+                ) {
+                  if (data.config.url !== 'auth/api/v1/refresh') {
+                    if (data.config.method === 'POST') {
+                      if (handleTokenInfo) {
+                        handleTokenInfo();
+                      }
+                      return;
+                    }
+                    if (data.config.method === 'DELETE') {
+                      if (handleTokenInfo) {
+                        handleTokenInfo();
+                      }
+                      return;
+                    }
+                    if (data.config.method === 'PUT') {
+                      if (handleTokenInfo) {
+                        handleTokenInfo();
+                      }
+                      return;
+                    }
+                  }
+                }
+              }
+            },
+          },
+          mutations: {
+            retry: 1,
+          },
+        },
+      }),
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {props.children}
+    </QueryClientProvider>
+  );
+};
+
+export default ReactQueryClientProvider;
