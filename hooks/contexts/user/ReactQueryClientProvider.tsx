f9856c2f6221aa31e76deeb46af6302225fd3bf1
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-14 15:07:26 +0900

    PTSD-14 Feat : ReactQueryClientProvider 추가 개발
    ReactQueryClientProvider 추가 개발 : mutation 시 onSuccess 일때 토큰 리프레쉬

--- hooks/contexts/user/ReactQueryClientProvider.tsx
+++ hooks/contexts/user/ReactQueryClientProvider.tsx
@@ -18,39 +18,17 @@ const ReactQueryClientProvider = (props: {
             refetchOnReconnect: false,
             refetchInterval: 1800000,
             retry: 1,
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onSuccess(data: any) {
-              if (router.pathname !== '/signin') {
-                if (
-                  data.config.url !==
-                  '/telemedicine/api/v1/telemedicine-clinic/status'
-                ) {
-                  if (data.config.url !== 'auth/api/v1/refresh') {
-                    if (data.config.method === 'POST') {
-                      if (handleTokenInfo) {
-                        handleTokenInfo();
-                      }
-                      return;
-                    }
-                    if (data.config.method === 'DELETE') {
-                      if (handleTokenInfo) {
-                        handleTokenInfo();
-                      }
-                      return;
-                    }
-                    if (data.config.method === 'PUT') {
-                      if (handleTokenInfo) {
-                        handleTokenInfo();
-                      }
-                      return;
-                    }
-                  }
-                }
-              }
-            },
           },
           mutations: {
             retry: 1,
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            onSuccess(data: any, variables, context) {
+              if (router.pathname !== '/signin') {
+                if (data.config.url !== 'auth/api/v1/refresh') {
+                  handleTokenInfo();
+                }
+              }
+            },
           },
         },
       }),
