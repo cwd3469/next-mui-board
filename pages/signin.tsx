d291b02f72ccc96e2f6a64b0c0c59b391156978f
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-14 10:46:32 +0900

    PTSD-14 Feat : Signin 추가 개발
    Signin 추가 개발 : UserInfoContext -> deleteToken 적용

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,7 +1,16 @@
 import AuthPage from '@components/auth/AuthPage';
 import { SigninInfoProvider } from '@hooks/contexts/info/SigninInfoContext';
+import { UserInfoContext } from '@hooks/contexts/user/UserInfoContext';
+import { useContext, useEffect } from 'react';
 
 const Signin = () => {
+  const { deleteToken } = useContext(UserInfoContext);
+
+  useEffect(() => {
+    if (deleteToken) {
+      deleteToken();
+    }
+  }, [deleteToken]);
   return (
     <SigninInfoProvider>
       <main>
