4a2728e99eb016e2275bd9a02ec66b84aa066531
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-22 10:58:22 +0900

    PTSD-159 Feat : SignupStepOne 추가
    SignupStepOne 추가 : SignupTerms , PrivacyPolicy , PrivacyAgree , PartnerAgree 적용

--- components/auth/signup/modal/SignupStepOne.tsx
+++ components/auth/signup/modal/SignupStepOne.tsx
@@ -9,6 +9,9 @@ import SignupTerms from './SignupTerms';
 import SignupStepTwo from './SignupStepTwo';
 import { makeStyles } from '@mui/styles';
 import { useDebounceFn } from 'ahooks';
+import PartnerAgree from '@components/auth/terms/PartnerAgree';
+import PrivacyAgree from '@components/auth/terms/PrivacyAgree';
+import PrivacyPolicy from '@components/auth/terms/PrivacyPolicy';
 
 const useStyles = makeStyles((theme) => ({
   subTitle: {
@@ -26,7 +29,9 @@ const SignupStepOne = (props: ModalType) => {
   const termHook = useTerms();
   const classes = useStyles();
   const [modalOn, setModalOn] = useState<boolean>(false);
-  const [termsOn, setTermsOn] = useState<boolean>(false);
+  const [partnerAgreeOn, setPartnerAgreeOn] = useState<boolean>(false);
+  const [privacyAgreeOn, setPrivacyAgreeOn] = useState<boolean>(false);
+  const [privacyPolicyOn, setPrivacyPolicyOn] = useState<boolean>(false);
   const [bgDisable, setBgDisable] = useState<boolean>(false);
 
   /** SignupStepOne off 기능*/
@@ -34,17 +39,31 @@ const SignupStepOne = (props: ModalType) => {
     termHook.onClickReset();
     props.handleClose();
     setBgDisable(false);
-    setTermsOn(false);
+    setPrivacyAgreeOn(false);
+    setPrivacyPolicyOn(false);
+    setPartnerAgreeOn(false);
     setModalOn(false);
   };
   /**SignupStepOne 약관 모달 on 기능 */
-  const onOpenTerms = () => {
-    setTermsOn(true);
+  const onTermsPartnerAgree = () => {
+    setPartnerAgreeOn(true);
+    setBgDisable(true);
+  };
+  /**SignupStepOne 약관 모달 on 기능 */
+  const onTermsPrivacyAgree = () => {
+    setPrivacyAgreeOn(true);
+    setBgDisable(true);
+  };
+  /**SignupStepOne 약관 모달 on 기능 */
+  const onTermsPrivacyPolicy = () => {
+    setPrivacyPolicyOn(true);
     setBgDisable(true);
   };
   /** SignupStepOne 약관 모달 off 기능*/
   const onCloseTerms = () => {
-    setTermsOn(false);
+    setPrivacyAgreeOn(false);
+    setPrivacyPolicyOn(false);
+    setPartnerAgreeOn(false);
     setBgDisable(false);
   };
   const handleEvent = useDebounceFn(
@@ -101,13 +120,13 @@ const SignupStepOne = (props: ModalType) => {
                 onChange={() => termHook.onCheckBoxA(termHook.agreeTermA)}
               />
               <WSubTitle
-                title="서비스 이용약관 동의"
+                title="개인정보 보호 의무 동의"
                 className={classes.subTitle}
                 onClick={() => termHook.onCheckBoxA(termHook.agreeTermA)}
                 require
               />
             </TermsGrid>
-            <TermsButton onClick={onOpenTerms}>전문보기</TermsButton>
+            <TermsButton onClick={onTermsPrivacyAgree}>전문보기</TermsButton>
           </TermsGrid>
           <TermsGrid padding={'24px 24px'}>
             <TermsGrid gap="16px">
@@ -116,13 +135,13 @@ const SignupStepOne = (props: ModalType) => {
                 onChange={() => termHook.onCheckBoxB(termHook.agreeTermB)}
               />
               <WSubTitle
-                title="개인정보 보호 의무 동의"
+                title="개인정보 처리 동의서"
                 className={classes.subTitle}
                 onClick={() => termHook.onCheckBoxB(termHook.agreeTermB)}
                 require
               />
             </TermsGrid>
-            <TermsButton onClick={onOpenTerms}>전문보기</TermsButton>
+            <TermsButton onClick={onTermsPrivacyPolicy}>전문보기</TermsButton>
           </TermsGrid>
           <TermsGrid padding={'24px 24px'}>
             <TermsGrid gap="16px">
@@ -131,17 +150,46 @@ const SignupStepOne = (props: ModalType) => {
                 onChange={() => termHook.onCheckBoxC(termHook.agreeTermC)}
               />
               <WSubTitle
-                title="개인정보 처리 방침 동의"
+                title="파트너사 이용약관"
                 className={classes.subTitle}
                 require
                 onClick={() => termHook.onCheckBoxC(termHook.agreeTermC)}
               />
             </TermsGrid>
-            <TermsButton onClick={onOpenTerms}>전문보기</TermsButton>
+            <TermsButton onClick={onTermsPartnerAgree}>전문보기</TermsButton>
           </TermsGrid>
         </Stack>
         <Box height="100px" />
-        <SignupTerms open={termsOn} handleClose={onCloseTerms} />
+        {/* 파트너사 이용약관을 조회 */}
+        {partnerAgreeOn ? (
+          <SignupTerms
+            open={partnerAgreeOn}
+            handleClose={onCloseTerms}
+            html={<PartnerAgree />}
+          />
+        ) : (
+          ''
+        )}
+        {/* 개인정보 처리에 관한 동의서를 조회 */}
+        {privacyAgreeOn ? (
+          <SignupTerms
+            open={privacyAgreeOn}
+            handleClose={onCloseTerms}
+            html={<PrivacyAgree />}
+          />
+        ) : (
+          ''
+        )}
+        {/* 파트너 개인정보 보호 의무 동의를 조회 */}
+        {privacyPolicyOn ? (
+          <SignupTerms
+            open={privacyPolicyOn}
+            handleClose={onCloseTerms}
+            html={<PrivacyPolicy />}
+          />
+        ) : (
+          ''
+        )}
         <SignupStepTwo open={modalOn} handleClose={onCloseModal} />
       </Stack>
     </WAlert>
