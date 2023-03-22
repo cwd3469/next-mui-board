beded456793cab94bafa3ff11d92b1b0b8158a5c
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-22 17:18:36 +0900

    PTSD-41 Feat : useNoticeList 변경
    useNoticeList 변경 : router query 추가

--- hooks/apis/notion/hooks/useNotice.tsx
+++ hooks/apis/notion/hooks/useNotice.tsx
@@ -1,18 +1,19 @@
 import { NoticeInterface } from '@components/notice/type';
-import { HistoryInterface } from '@components/preparation/history/type';
-import { FilterNoticeType } from '@hooks/contexts/filters/type';
 import useCodeMsgBundle from '@hooks/utils/useCodeMsgBundle';
 import { useToastContext } from '@hooks/utils/useToastContext';
 import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { apiNoticeList } from '..';
 import { NOTICELIST } from '../queryKey';
+import { useRouter } from 'next/router';
 
-const useNoticeList = (parms: FilterNoticeType) => {
+const useNoticeList = () => {
+  const router = useRouter();
+  const query = router.query;
   const toast = useToastContext();
   const msg = useCodeMsgBundle();
-  const { data, isError, isLoading } = useQuery(NOTICELIST(parms), async () => {
-    return await apiNoticeList({ filter: parms });
+  const { data, isError, isLoading } = useQuery(NOTICELIST(query), async () => {
+    return await apiNoticeList(query);
   });
   const code = data?.data.code;
   const noticeData: NoticeInterface[] = isLoading
