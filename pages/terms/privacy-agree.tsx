1f0a0fd18649412a2dde99738afe0955a8f65ac1
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-22 10:56:42 +0900

    PTSD-159 Feat : PrivacyAgreePage 추가
    PrivacyAgreePage 추가 : PrivacyAgree 적용

--- pages/terms/privacy-agree.tsx
+++ pages/terms/privacy-agree.tsx
@@ -1,18 +1,7 @@
-import { apiPrivacyUsageAgreement } from '@hooks/apis/myinfo';
-import { useEffect, useState } from 'react';
+import PrivacyAgree from '@components/auth/terms/PrivacyAgree';
 
-const PrivacyAgree = () => {
-  const [html, setHtml] = useState<string>('');
-  useEffect(() => {
-    apiPrivacyUsageAgreement()
-      .then((data) => {
-        setHtml(data.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
+const PrivacyAgreePage = () => {
+  return <PrivacyAgree />;
 };
 
-export default PrivacyAgree;
+export default PrivacyAgreePage;
