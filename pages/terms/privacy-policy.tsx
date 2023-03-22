ed6305a06c436ccc891422553a89439d73cd1ec5
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-22 10:56:58 +0900

    PTSD-159 Feat : PrivacyPolicyPage 추가
    PrivacyPolicyPage 추가 : PrivacyPolicy 적용

--- pages/terms/privacy-policy.tsx
+++ pages/terms/privacy-policy.tsx
@@ -1,18 +1,7 @@
-import { apiPrivacyPolicy } from '@hooks/apis/myinfo';
-import { useEffect, useState } from 'react';
+import PrivacyPolicy from '@components/auth/terms/PrivacyPolicy';
 
-const PrivacyPolicy = () => {
-  const [html, setHtml] = useState<string>('');
-  useEffect(() => {
-    apiPrivacyPolicy()
-      .then((data) => {
-        setHtml(data.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
+const PrivacyPolicyPage = () => {
+  return <PrivacyPolicy />;
 };
 
-export default PrivacyPolicy;
+export default PrivacyPolicyPage;
