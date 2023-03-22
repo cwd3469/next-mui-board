763b42041fb2061b19811cda5523694af763ffb2
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-22 13:31:20 +0900

    PTSD-159 Feat : Notice 추가
    Notice 추가 dynamic import 설정

--- pages/notice/index.tsx
+++ pages/notice/index.tsx
@@ -1,7 +1,11 @@
 import { Gnb } from '@components/common/layouts/gnb/Gnb';
 import { WLayout } from '@components/common/layouts/WLayout';
-import NoticePage from '@components/notice/NoticePage';
 import { NoticeFilterProvider } from '@hooks/contexts/filters/NoticeFilterContext';
+import dynamic from 'next/dynamic';
+
+const NoticePage = dynamic(() => import('../../components/notice/NoticePage'), {
+  loading: () => <p>Loading...</p>,
+});
 
 const Notice = () => {
   return (
