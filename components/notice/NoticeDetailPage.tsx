45c58aa659a7afbb9e1ca782988367b1b7dbc3fd
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-23 09:20:58 +0900

    PTSD-41 Feat : NoticeDetailPage 추가
    NoticeDetailPage 추가 : LoadingErrorFallback 적용

--- components/notice/NoticeDetailPage.tsx
+++ components/notice/NoticeDetailPage.tsx
@@ -1,74 +1,81 @@
 import { WBoxLayout } from '@components/common/layouts/WLayout';
 import useNoticeDetail from '@hooks/apis/notion/hooks/useNoticeDetail';
-import { Box, Grid, Stack, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { useRouter } from 'next/router';
-import { WNotionBoardBody, WNotionBoardHead } from './styled';
+import { WNotionBoardHead } from './styled';
 import { ellipsis } from '@styles/common';
 import { FlexCenterC, FlexStartC, FlexStartR } from '@styles/flexGrid';
-import { NoticeDetailInterface } from './type';
 import WNoticeTextarea from '@components/common/inputs/Textarea/modules/WNoticeTextarea';
 import { WButton } from '@components/common/button/WButton';
 import { useCallback } from 'react';
 import { dateFormat } from '@utils/date';
+import LoadingErrorFallback from '@components/common/api/LoadingErrorFallback';
 
-const NoticeDetailPage = () => {
+const NoticeDetailPage = (props: { id: string }) => {
   const router = useRouter();
   const onClickBack = useCallback(() => {
+    const storage = globalThis?.sessionStorage;
+    console.log(storage);
+    if (storage.prevPath === storage.currentPath) {
+      router.push('/notice');
+      return;
+    }
+    if (storage.prevPath) {
+      router.back();
+      return;
+    }
     router.push('/notice');
   }, [router]);
-  const { data, isError, isLoading } = useNoticeDetail(
-    router.query.id ? (router.query.id as string) : '0',
-  );
+
+  const { data, isError, isLoading, isWarning } = useNoticeDetail(props.id);
 
   return (
     <FlexCenterC gap="26px">
       <WBoxLayout width="100%">
-        {isLoading ? (
-          <>isLoading...</>
-        ) : isError ? (
-          <>isError...</>
-        ) : data ? (
-          data.data.code === '0000' ? (
-            <>
-              <WNotionBoardHead>
-                <FlexStartC gap="12px">
-                  <Typography variant="h6" className="title" sx={ellipsis}>
-                    {data.data.data.title}
-                  </Typography>
-                  <FlexStartR gap="16px">
-                    <FlexStartR gap="10px">
-                      <Typography variant="subtitle2" className="info name">
-                        구분
-                      </Typography>
-                      <Typography
-                        variant="subtitle2"
-                        className="info substance"
-                      >
-                        {data.data.data.type}
-                      </Typography>
+        <LoadingErrorFallback
+          data={data}
+          isError={isError}
+          isLoading={isLoading}
+          isWarning={isWarning}
+          contexts={(info) => {
+            return (
+              <>
+                <WNotionBoardHead>
+                  <FlexStartC gap="12px">
+                    <Typography variant="h6" className="title" sx={ellipsis}>
+                      {info.data.data.title}
+                    </Typography>
+                    <FlexStartR gap="16px">
+                      <FlexStartR gap="10px">
+                        <Typography variant="subtitle2" className="info name">
+                          구분
+                        </Typography>
+                        <Typography
+                          variant="subtitle2"
+                          className="info substance"
+                        >
+                          {info.data.data.type}
+                        </Typography>
+                      </FlexStartR>
+                      <FlexStartR gap="10px">
+                        <Typography variant="subtitle2" className="info name">
+                          작성일
+                        </Typography>
+                        <Typography
+                          variant="subtitle2"
+                          className="info substance"
+                        >
+                          {dateFormat(info.data.data.createdAt).day}
+                        </Typography>
+                      </FlexStartR>
                     </FlexStartR>
-                    <FlexStartR gap="10px">
-                      <Typography variant="subtitle2" className="info name">
-                        작성일
-                      </Typography>
-                      <Typography
-                        variant="subtitle2"
-                        className="info substance"
-                      >
-                        {dateFormat(data.data.data.createdAt).day}
-                      </Typography>
-                    </FlexStartR>
-                  </FlexStartR>
-                </FlexStartC>
-              </WNotionBoardHead>
-              <WNoticeTextarea value={data.data.data.content} />
-            </>
-          ) : (
-            <></>
-          )
-        ) : (
-          <></>
-        )}
+                  </FlexStartC>
+                </WNotionBoardHead>
+                <WNoticeTextarea value={info.data.data.content} />
+              </>
+            );
+          }}
+        />
       </WBoxLayout>
       <WButton
         onClick={onClickBack}
