4879ba9b4a1d436ef57c45e269b6db651ec2bebc
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-03-29 15:39:41 +0900

    PTSD-204 Feat : useListRequest 변경
    useListRequest 변경 : apiProceedList 적용 , useCodeWarningEffect 적용

--- hooks/apis/preparation/request/hooks/useListRequest.tsx
+++ hooks/apis/preparation/request/hooks/useListRequest.tsx
@@ -1,62 +1,25 @@
-import { RequestInterface } from '@components/preparation/request/type';
-import { FilterListData } from '@hooks/contexts/filters/type';
-import useCodeMsgBundle from '@hooks/utils/useCodeMsgBundle';
-import { useToastContext } from '@hooks/utils/useToastContext';
-import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { apiProceedList } from '..';
-import { HISTORYLIST } from '../queryKey';
+import { REQUEST_LIST } from '../queryKey';
 import useCodeWarningEffect from '@hooks/utils/useCodeWarningEffect';
-import { AxiosResponse } from 'axios';
 import { ParsedUrlQuery } from 'querystring';
-import { transQueryUrl } from '@utils/transtext';
+import { transQueryDateToString, transQueryUrl } from '@utils/transtext';
 
 const useListRequest = (query: ParsedUrlQuery) => {
-  useEffect(() => {
-    // const queryUrl = transQueryUrl(query);
-    // console.log('=============');
-    // console.log(queryUrl);
-    // console.log(query);
-    // console.log('=============');
-  }, [query]);
-  // const { data, isError, isLoading } = useQuery(
-  //   HISTORYLIST(parms),
-  //   async () => {
-  //     return await apiProceedList(parms);
-  //   },
-  // );
-  // const code = data?.data.code;
+  const queryUrl = transQueryUrl(query);
+  const queryDate = transQueryDateToString(query, true);
+  const queryString = `${queryUrl}${queryDate}`;
 
-  // const { isWarning } = useCodeWarningEffect({ code: code });
-  // info.data.data.page.content
-  const data: any = {
-    data: {
-      code: '0000',
-      data: {
-        page: {
-          content: [
-            {
-              ulid: '1C2Y32Y0QSD3421C2Y0QSDTEST',
-              status: 'PAYMENT_WAITING',
-              statusNameKo: '결제 대기',
-              deliveryForm: 'DELIVERY',
-              deliveryFormKo: '택배 배송',
-              waybillNumber: '',
-              courier: '',
-              completionAt: '2023-01-16T17:22:44',
-              treatHospitalName: '마인드힐링정신 건강의학과의원',
-              treatDoctorName: '홍길동',
-              treatHospitalPhone: '023004200',
-            },
-          ],
-          totalPages: 0,
-        },
-      },
+  const { data, isError, isLoading } = useQuery(
+    REQUEST_LIST(query),
+    async () => {
+      return await apiProceedList(queryString);
     },
-  };
-  const isError = false;
-  const isLoading = false;
-  const isWarning = false;
+  );
+  const code = data?.data.code;
+
+  const { isWarning } = useCodeWarningEffect({ code: code });
+
   return { data, isError, isLoading, isWarning };
 };
 
