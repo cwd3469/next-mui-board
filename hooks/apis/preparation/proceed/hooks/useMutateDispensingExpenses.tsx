4034872dacedfd1cdfff6cb41158201a194aa121
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-04-03 14:29:18 +0900

    PTSD-202 Feat : useMutateDispensingExpenses 추가
    useMutateDispensingExpenses 추가 : props 추가

--- hooks/apis/preparation/proceed/hooks/useMutateDispensingExpenses.tsx
+++ hooks/apis/preparation/proceed/hooks/useMutateDispensingExpenses.tsx
@@ -1,16 +1,18 @@
-import { PreparationRequestDto } from '@components/preparation/history/type';
 import useCodeMsgBundle from '@hooks/utils/useCodeMsgBundle';
 import { useToastContext } from '@hooks/utils/useToastContext';
-import { useCallback, useEffect } from 'react';
-import { useMutation, useQuery } from 'react-query';
+import { useCallback } from 'react';
+import { useMutation } from 'react-query';
 import { apiDispensingExpenses } from '..';
-import { PREPARATIONREQUEST } from '../queryKey';
+import { commaRemove } from '@utils/formatNumber';
 
-interface UseDispensingExpensesType {
-  dispensingExpenses: string;
+export interface UseDispensingExpensesType {
+  medicineCost: string;
+  medicineOrderUlid: string;
+  onError?: () => void;
+  onSuccess?: () => void;
 }
 const useMutateDispensingExpenses = (props: UseDispensingExpensesType) => {
-  const { dispensingExpenses } = props;
+  const { medicineCost, onError, onSuccess } = props;
   const toast = useToastContext();
   const msg = useCodeMsgBundle();
   const { mutate: mutateDispensingExpenses } = useMutation(
@@ -18,14 +20,20 @@ const useMutateDispensingExpenses = (props: UseDispensingExpensesType) => {
   );
 
   const onClickDispensingExpenses = useCallback(() => {
-    if (dispensingExpenses) {
-      mutateDispensingExpenses(dispensingExpenses, {
+    if (medicineCost) {
+      const dto = {
+        medicineCost: commaRemove(props.medicineCost),
+        medicineOrderUlid: props.medicineOrderUlid,
+      };
+      mutateDispensingExpenses(dto, {
         onSuccess: (res) => {
           const code = res.data.code;
-          const data = res.data.data;
           if (code !== '0000') {
             toast?.on(msg.errMsg(code), 'info');
           } else {
+            if (onSuccess) {
+              onSuccess();
+            }
             return;
           }
         },
@@ -34,10 +42,22 @@ const useMutateDispensingExpenses = (props: UseDispensingExpensesType) => {
             `조제비 수정이 실패하였습니다 \n잠시 후, 다시 시도해 주세요`,
             'error',
           );
+          if (onError) {
+            onError();
+          }
         },
       });
     }
-  }, [dispensingExpenses, msg, mutateDispensingExpenses, toast]);
+  }, [
+    medicineCost,
+    msg,
+    mutateDispensingExpenses,
+    onError,
+    onSuccess,
+    props.medicineCost,
+    props.medicineOrderUlid,
+    toast,
+  ]);
 
   return { onClickDispensingExpenses };
 };
