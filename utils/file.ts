f26221ce2ca2580f4b58b1a17efd4a5297091e64
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-04-03 10:22:06 +0900

    PTSD-202 Feat : b64DecodeUnicode , b64EncodeUnicode 기능 추가
    b64DecodeUnicode , b64EncodeUnicode 기능 추가 : Encoding UTF8 ⇢ base64 , Decoding base64 ⇢ UTF8

--- utils/file.ts
+++ utils/file.ts
@@ -7,6 +7,27 @@ export function blobToFile(theBlob: Blob, fileName: string) {
     type: theBlob.type,
   });
 }
+
+// Encoding UTF8 ⇢ base64
+export function b64EncodeUnicode(str: string) {
+  return btoa(
+    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+      return String.fromCharCode(parseInt(p1, 16));
+    }),
+  );
+}
+
+// Decoding base64 ⇢ UTF8
+export function b64DecodeUnicode(str: string) {
+  return decodeURIComponent(
+    Array.prototype.map
+      .call(atob(str), function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join(''),
+  );
+}
+
 // image 파일 만들기
 export const convertURLtoFile = async (url: string) => {
   if (typeof window !== 'undefined') {
