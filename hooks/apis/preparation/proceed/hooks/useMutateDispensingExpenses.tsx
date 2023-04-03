e8acbd21431c51c5a15720b28cdc3aa507337868
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-04-03 15:26:07 +0900

    PTSD-202 Feat : 불필요한 코드 삭제
    불필요한 코드 삭제 : useMutateDispensingExpenses 추가

--- hooks/apis/preparation/proceed/hooks/useMutateDispensingExpenses.tsx
+++ hooks/apis/preparation/proceed/hooks/useMutateDispensingExpenses.tsx
@@ -2,28 +2,32 @@ import useCodeMsgBundle from '@hooks/utils/useCodeMsgBundle';
 import { useToastContext } from '@hooks/utils/useToastContext';
 import { useCallback } from 'react';
 import { useMutation } from 'react-query';
-import { apiDispensingExpenses } from '..';
+import { apiDispensingExpenses, apiPrepared } from '..';
 import { commaRemove } from '@utils/formatNumber';
 
 export interface UseDispensingExpensesType {
-  medicineCost: string;
+  medicineCost?: string;
   medicineOrderUlid: string;
   onError?: () => void;
   onSuccess?: () => void;
 }
 const useMutateDispensingExpenses = (props: UseDispensingExpensesType) => {
-  const { medicineCost, onError, onSuccess } = props;
+  const { medicineCost, medicineOrderUlid, onError, onSuccess } = props;
   const toast = useToastContext();
   const msg = useCodeMsgBundle();
+  /**useMutateDispensingExpenses 조제비 수정  useMutation*/
   const { mutate: mutateDispensingExpenses } = useMutation(
     apiDispensingExpenses,
   );
+  /**useMutateDispensingExpenses 조제 완료  useMutation*/
+  const { mutate: mutationPrepared } = useMutation(apiPrepared);
 
+  /**useMutateDispensingExpenses 조제비 수정  기능*/
   const onClickDispensingExpenses = useCallback(() => {
     if (medicineCost) {
       const dto = {
-        medicineCost: commaRemove(props.medicineCost),
-        medicineOrderUlid: props.medicineOrderUlid,
+        medicineCost: commaRemove(medicineCost),
+        medicineOrderUlid: medicineOrderUlid,
       };
       mutateDispensingExpenses(dto, {
         onSuccess: (res) => {
@@ -50,16 +54,45 @@ const useMutateDispensingExpenses = (props: UseDispensingExpensesType) => {
     }
   }, [
     medicineCost,
+    medicineOrderUlid,
     msg,
     mutateDispensingExpenses,
     onError,
     onSuccess,
-    props.medicineCost,
-    props.medicineOrderUlid,
     toast,
   ]);
+  /**useMutateDispensingExpenses 조제 완료 기능*/
+  const onClickPreparationComplete = useCallback(() => {
+    if (medicineOrderUlid) {
+      const dto = {
+        medicineOrderUlid: medicineOrderUlid,
+      };
+      mutationPrepared(dto, {
+        onSuccess: (res) => {
+          const code = res.data.code;
+          if (code !== '0000') {
+            toast?.on(msg.errMsg(code), 'info');
+          } else {
+            if (onSuccess) {
+              onSuccess();
+            }
+            return;
+          }
+        },
+        onError: (errMsg) => {
+          toast?.on(
+            `조제 완료가 실패하였습니다 \n잠시 후, 다시 시도해 주세요`,
+            'error',
+          );
+          if (onError) {
+            onError();
+          }
+        },
+      });
+    }
+  }, [medicineOrderUlid, msg, mutationPrepared, onError, onSuccess, toast]);
 
-  return { onClickDispensingExpenses };
+  return { onClickDispensingExpenses, onClickPreparationComplete };
 };
 
 export default useMutateDispensingExpenses;
