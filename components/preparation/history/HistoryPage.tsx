4f13e32c30e4c1d9ccc882a48dde9929478496ab
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-04-04 17:29:51 +0900

    PTSD-202 Feat : HistoryPage 추가
    HistoryPage 추가 : loadingErrorFallbackList 추가 로직 변경

--- components/preparation/history/HistoryPage.tsx
+++ components/preparation/history/HistoryPage.tsx
@@ -2,31 +2,43 @@ import WPagination from '@components/common/button/WPagination';
 import useListHistory from '@hooks/apis/preparation/history/hooks/useListHistory';
 import { HistoryFilterContext } from '@hooks/contexts/filters/HistoryFilterContext';
 import { Stack } from '@mui/material';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import HistoryFilter from './modules/HistoryFilter';
 import HistoryStatusNoti from './modules/HistoryStatusNoti';
 import HistoryTable from './modules/HistoryTable';
+import { loadingErrorFallbackList } from '@components/common/api/LoadingErrorFallback';
 
 const HistoryPage = () => {
-  const { filter, date, setInFilter } = useContext(HistoryFilterContext);
-  const { historyListData, totalPages } = useListHistory({ filter, date });
+  const { filter, setInFilter } = useContext(HistoryFilterContext);
+  const { data, isError, isLoading, isWarning } = useListHistory();
 
   const pagination = (event: React.ChangeEvent<unknown>, value: number) => {
-    setInFilter(value, 'page');
+    setInFilter(value - 1, 'page');
   };
+
+  const info = loadingErrorFallbackList({
+    data: data,
+    isError: isError,
+    isLoading: isLoading,
+    isWarning: isWarning,
+  });
 
   return (
     <Stack gap="20px">
       <Stack gap="10px">
-        <HistoryStatusNoti />
+        <HistoryStatusNoti
+          totalDeliveryPrepareCount={info.totalDeliveryPrepareCount}
+          totalMedicineCompleteCount={info.totalMedicineCompleteCount}
+          totalMedicineCost={info.totalMedicineCost}
+        />
         <HistoryFilter />
       </Stack>
-      <HistoryTable data={historyListData} />
+      <HistoryTable data={info.contents} />
       <WPagination
         paddingTop="4px"
-        page={filter.page}
+        page={filter.page + 1}
         pagination={pagination}
-        count={totalPages}
+        count={info.totalPages}
       />
     </Stack>
   );
