82a58733cb04fb75fb9c105b479d5eb3d42c8a6a
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-04-05 09:10:31 +0900

    PTSD-303 Feat : WSelectPreparation 변경
    WSelectPreparation 변경 : option 변경

--- components/common/inputs/select/modules/WSelectPreparation.tsx
+++ components/common/inputs/select/modules/WSelectPreparation.tsx
@@ -1,20 +1,26 @@
 import { SelectChangeEvent } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import WSelect, { OptionType } from '..';
 
 const WSelectPreparation = (props: {
+  value?: string;
   callBack: (id: string, keyId: string) => void;
 }) => {
   const option: OptionType[] = [
-    { id: 'DEFULT', name: '상태 전체' },
-    { id: 'CANCEL', name: '취소' },
+    { id: 'COMPLETED', name: '상태 완료' },
+    { id: 'REFUSE', name: '취소' },
   ];
   const [state, setState] = useState<string>(option[0].id);
   const onSelectOption = (event: SelectChangeEvent) => {
     const value = event.target.value;
     setState(value);
-    props.callBack(value, 'preparationStatus');
+    props.callBack(value, 'medicineStatus');
   };
+  useEffect(() => {
+    if (props.value) {
+      setState(props.value);
+    }
+  }, [props.value]);
   return (
     <WSelect
       name={'wSelectPreparation'}
