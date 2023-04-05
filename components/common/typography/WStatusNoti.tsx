d49e0db61ce8252227cefcf95238a752c14e51d6
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-04-05 11:36:10 +0900

    PTSD-303 Feat : WStatusNoti 추가
    WStatusNoti 추가 : Grid 추가

--- components/common/typography/WStatusNoti.tsx
+++ components/common/typography/WStatusNoti.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, styled, Typography } from '@mui/material';
+import { Box, Grid, styled, SxProps, Theme, Typography } from '@mui/material';
 
 export const WNotiTypography = styled(Typography)(({ theme }) => ({
   '&.title': {
@@ -26,13 +26,16 @@ const WStatusNoti = (props: {
   title: string;
   counting: string;
   units: string;
+  sx?: SxProps<Theme>;
 }) => {
   return (
-    <Grid container width="auto" alignItems="center" gap="1px">
+    <Grid container width="auto" alignItems="center" gap="1px" sx={props.sx}>
       <WNotiTypography className="title">{props.title}</WNotiTypography>
       <Box width="12px" />
-      <WNotiTypography className="counting">{props.counting}</WNotiTypography>
-      <WNotiTypography className="units">{props.units}</WNotiTypography>
+      <Grid container width="auto" alignItems="center" gap="3px">
+        <WNotiTypography className="counting">{props.counting}</WNotiTypography>
+        <WNotiTypography className="units">{props.units}</WNotiTypography>
+      </Grid>
     </Grid>
   );
 };
