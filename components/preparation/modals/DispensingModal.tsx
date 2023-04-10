f70b2d3d843f5b5aa4a7feac1f166693d6a53b8e
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-04-10 10:58:07 +0900

    PTSD-303 Feat : DispensingModal 추가
    DispensingModal 추가 : props mode 추가

--- components/preparation/modals/DispensingModal.tsx
+++ components/preparation/modals/DispensingModal.tsx
@@ -5,16 +5,17 @@ import processStatus from 'public/assets/icon/processStatus.svg';
 import Image from 'next/image';
 import useMutateDispensingExpenses from '@hooks/apis/preparation/proceed/hooks/useMutateDispensingExpenses';
 import { useDebounceFn, useKeyPress } from 'ahooks';
-import DeliveryRequestModal from './DeliveryRequestModal';
+import DeliveryRequestModal, { DeliveryState } from './DeliveryRequestModal';
 import { useCallback, useState } from 'react';
 import FailedChangeModal from './FailedChangeModal';
 
 interface DispensingModalType extends ModalType {
   id: string;
+  mode?: 'history' | 'proceed';
 }
 
 const DispensingModal = (props: DispensingModalType) => {
-  const { open, handleClose, id } = props;
+  const { open, handleClose, id, mode } = props;
   /**DispensingModal 당일배송 요청 모달 상태*/
   const [deliveryOpen, setDeliveryOpen] = useState<boolean>(false);
   const [errorOpen, setErrorOpen] = useState<boolean>(false);
@@ -61,7 +62,11 @@ const DispensingModal = (props: DispensingModalType) => {
     <WConfirm
       activeOn
       open={open}
-      title={'배송 요청 전 복약지도 안내'}
+      title={
+        mode === 'history'
+          ? '완료된 조제건 배송 요청 안내'
+          : '배송 요청 전 복약지도 안내'
+      }
       maxWidth="sm"
       titleSx={{ padding: '50px 0 60px' }}
       btnTitle={'배송 요청'}
@@ -74,10 +79,14 @@ const DispensingModal = (props: DispensingModalType) => {
         <>
           <Stack alignItems="center">
             <Typography variant="h5" color="#666" fontWeight="400">
-              {`환자에게 유선 및 서면을 통한 `}
+              {mode === 'history'
+                ? '완료된 조제건에 대해'
+                : `환자에게 유선 및 서면을 통한 `}
             </Typography>
             <Typography variant="h5" color="#666" fontWeight="400">
-              {`복약지도 꼭! 부탁드립니다. `}
+              {mode === 'history'
+                ? '사용자에게 배송 요청을 해주세요.'
+                : `복약지도 꼭! 부탁드립니다. `}
             </Typography>
           </Stack>
         </>
