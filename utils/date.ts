8ef683693d34bda0e93026ec8091343cae0b2304
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-04-25 17:06:47 +0900

    PTSD-398 Feat : timeListFn 기능 개발
    timeListFn 기능 개발 : 타임라인 리스트 생성 기능 개발

--- utils/date.ts
+++ utils/date.ts
@@ -1,3 +1,4 @@
+import { OptionType } from '@components/common/inputs/select';
 import dayjs from 'dayjs';
 
 export const stringToDate = (txt: string) => {
@@ -25,4 +26,59 @@ export const dateFormat = (date: string) => {
 export const stringToDey = (txt: string) => {
   const day = dayjs(txt);
   return day;
+};
+
+export interface WTimeListOption extends OptionType {
+  index: number;
+}
+
+export const timeListFn = (param: {
+  start: string;
+  end: string;
+  Interval: number;
+  viewFormat?: string;
+  ListFormat?: string;
+}): WTimeListOption[] => {
+  const { start, end, Interval, viewFormat, ListFormat } = param;
+  const startDate = dayjs(`2023-04-12T${start}:00:00`);
+  const endDate = dayjs(`2023-04-12T${end}:00:00`);
+  const intervalMinutes = 10;
+  const timeList: WTimeListOption[] = [];
+  let index = 1;
+  for (
+    let currentTime = startDate;
+    currentTime.isBefore(endDate);
+    currentTime = currentTime.add(intervalMinutes, 'minute')
+  ) {
+    const startTime = currentTime.format(viewFormat ? viewFormat : 'HH:mm');
+    const endTime = currentTime.add(Interval, 'm').format('HH:mm');
+    const itemName = `${startTime} ~ ${
+      endTime === '00:00' ? '24:00' : endTime
+    }`;
+    const itemId = JSON.stringify({
+      startTime: currentTime.format('HHmm'),
+      endTime: currentTime.add(Interval, 'm').format('HHmm'),
+    });
+    const rangeItem: WTimeListOption = {
+      index: index,
+      name: itemName,
+      id: itemId,
+    };
+    const singleItem: WTimeListOption = {
+      index: index,
+      name: startTime,
+      id: currentTime.format('HHmm'),
+    };
+    index++;
+    const item = () => {
+      switch (ListFormat) {
+        case 'range':
+          return rangeItem;
+        default:
+          return singleItem;
+      }
+    };
+    timeList.push(item());
+  }
+  return timeList;
 };
